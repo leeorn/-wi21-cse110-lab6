d303d269061acba05a5325c19ceb39b48ed68d91
leeorn <padiii@gmail.com>
2021-02-10 06:09:03 -0800

    fetch the data and store it

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,21 @@
 // Script.js
+let itemsInfo; 
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  // if not in the local storage yet
+  if(window.localStorage.getItem("data") == undefined){
+    fetch("https://fakestoreapi.com/products")
+      .then(function(response){return response.json()})
+      .then(data => {
+        // store the data in local storage
+        itemsInfo = data;
+        window.localStorage.setItem("data", JSON.stringify(itemsInfo));
+      })
+  }
+  else{
+    // load the iteams' data from the local storage
+    itemsInfo = JSON.parse(window.localStorage.getItem("data"))
+    // console.log(itemsInfo[0])
+  }
+
 });
