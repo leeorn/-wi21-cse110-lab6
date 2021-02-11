215d2e139775b013719a5d253581c9b436b781bb
leeorn <padiii@gmail.com>
2021-02-11 13:42:39 -0800

    working on custom component

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,33 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+  // Specify observed attributes so that
+  // attributeChangedCallback will work
+  static get observedAttributes() {
+    return ['itemAdded', 'title', 'price', 'src'];
+  }
+
+  constructor(){
+    super();
+
+    // Create a shadow root
+    const shadow = this.attachShadow({mode: 'open'});
+
+    // the html components
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    const title = document.createElement('p');
+    const price = document.createElement('p');
+
+    // connect the components with their classes, etc.
+    li.setAttribute('class', 'product');
+    title.setAttribute('class', 'title');
+    price.setAttribute('class', 'price');
+    img.setAttribute('src', '');
+  
+    
+
+  }
 }
 
 customElements.define('product-item', ProductItem);
